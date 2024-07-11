7b04c70423250578772ccff668000fed4d51f525
dbuchko <dbuchko@vmware.com>
2024-07-11 10:28:00 -0400

    Upgrade spring-boot from 2.7.x to 3.0.x

--- src/main/java/com/example/springboot/Application.java
+++ src/main/java/com/example/springboot/Application.java
@@ -7,8 +7,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
-import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
+import org.springframework.boot.actuate.web.exchanges.HttpExchangeRepository;
+import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository;
 
 @SpringBootApplication
 public class Application {
@@ -33,7 +33,7 @@ public class Application {
 	}
 	
     	@Bean
-	public HttpTraceRepository htttpTraceRepository() {
-		return new InMemoryHttpTraceRepository();
+	public HttpExchangeRepository htttpExchangeRepository() {
+		return new InMemoryHttpExchangeRepository();
 	}
 }
