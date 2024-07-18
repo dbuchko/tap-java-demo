0f876937afd3901d11cc633f55421591e49f319c
dbuchko <dbuchko@vmware.com>
2024-07-18 18:07:54 -0400

    Revert "Change listening port to 8081"
    Reverts commit 1ee5c8220ad2b6e5c9c6918b83ecacacd24f062f.

--- src/main/java/com/example/springboot/Application.java
+++ src/main/java/com/example/springboot/Application.java
@@ -7,8 +7,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.boot.actuate.web.exchanges.HttpExchangeRepository;
-import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 
 @SpringBootApplication
 public class Application {
@@ -33,7 +33,7 @@ public class Application {
 	}
 	
     	@Bean
-	public HttpExchangeRepository htttpExchangeRepository() {
-		return new InMemoryHttpExchangeRepository();
+	public HttpTraceRepository htttpTraceRepository() {
+		return new InMemoryHttpTraceRepository();
 	}
 }
